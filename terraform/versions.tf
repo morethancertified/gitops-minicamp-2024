a2e52ea39b99120abc4bff6e966dfbdc2f8af45c
Derek Morgan <79756941+morethancertified@users.noreply.github.com>
2024-10-15 11:10:43 -0500

    Added S3 Backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,9 @@
-terraform {}
+terraform {
+  required_version = "~> 1.9.5"
+  backend "s3" {
+    bucket         = "gitops-tf-backend"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "GitopsTerraformLocks"
+  }
+}
